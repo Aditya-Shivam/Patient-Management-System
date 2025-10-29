bb78d6f85cebc7f4f983f1847b1d58b680828e71
Aditya Shivam <adishivoff@gmail.com>
2025-10-30 01:33:07 +0530

    Added token check before hitting routes

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -2,22 +2,24 @@ package com.pm.authservice.controller;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @Operation(summary = "Generate token on user login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
-
         Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
         if(tokenOptional.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
@@ -25,6 +27,18 @@ public class AuthController {
 
         String token = tokenOptional.get();
         return new ResponseEntity<>(new LoginResponseDTO(token),HttpStatus.OK);
+    }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping ("/validate")
+    public ResponseEntity<String> validateToken(@RequestHeader("Authorization") String authHeader) {
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return authService.validate(authHeader.substring(7))
+                ? new ResponseEntity<>("SUCCESS", HttpStatus.OK)
+                : new ResponseEntity<>("FAILED", HttpStatus.UNAUTHORIZED);
+
     }
 
 }
--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,6 +2,8 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,13 +20,26 @@ public class AuthService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+//        System.out.println(loginRequestDTO.getPassword() + "_____" + userService.findByEmail(loginRequestDTO.getEmail()).orElse(new User()).getPassword());
         Optional<String> token = userService
                 .findByEmail(loginRequestDTO.getEmail())
-                .filter(u -> passwordEncoder.matches(u.getPassword(),loginRequestDTO.getPassword()))
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),u.getPassword()))
                 .map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
 
         return token;
 
+    }
+
+    public boolean validate(String token) {
+        try{
+           jwtUtil.validate(token);
+           return true;
+        } catch (JwtException ex){
+            return false;
+        }
     }
 }
